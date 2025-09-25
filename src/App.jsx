6e77010d65898b223a5e027c491f6b9cb4924881
aleem <sheikhaleem363@gmail.com>
2025-09-25 11:45:42 +0530

    Basic project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex bg-amber-300 items-center text-3xl">
+        Hey, Welcome!
+      </div>
+    </>
+  );
+}
+
+export default App;
